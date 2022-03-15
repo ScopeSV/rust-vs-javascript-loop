2bf556b8c1a9aff40d69dd00be474716c8dbd78e
Stephan B Valois <83061036+sbvalois@users.noreply.github.com>
2022-03-15 15:07:48 +0100

    wasm

--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -1,6 +1,5 @@
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
-// use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize)]
 struct Obj {
@@ -8,19 +7,21 @@ struct Obj {
 }
 
 #[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(typescript_type = "Array<string, number>")]
+    type MyArray;
+}
+
+#[wasm_bindgen]
 pub fn create_rust_objects(amount: usize) -> JsValue {
-    //let mut arr : Vec<HashMap<&str, usize>> = vec![];
-    let mut arr : Vec<Obj> = vec![];
+    let mut arr: Vec<Obj> = vec![];
     arr.reserve(amount);
 
     for i in 0..amount {
-/*         let mut field = HashMap::new();
-        field.insert("index", i);
-        arr.push(field) */
-
         let itm = Obj { index: i };
         arr.push(itm);
     }
 
     JsValue::from_serde(&arr).unwrap()
 }
+
